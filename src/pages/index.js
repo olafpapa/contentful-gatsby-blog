7239bd054b343ab618440ad4ad44a591f2cc67dd
olafpapa <s.iwatsuru@gmail.com>
2019-08-22 16:46:42 +0900

    Hero 削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ class RootIndex extends React.Component {
     return (
       <div style={{ background: '#fff' }}>
         <Helmet title={siteTitle} />
-        <Hero data={author.node} />
+        {/*<Hero data={author.node} />*/}
         <div className="wrapper">
           <h2 className="section-headline">Recent articles</h2>
           <ul className="article-list">
